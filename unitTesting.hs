e4a76e99b2744c7ccb16b26c7f0c59d36e78bf3e
giatrakosg <sdi1600036@di.uoa.gr>
2018-02-11 18:33:39 +0200

    Added unitTests

--- unitTesting.hs
+++ unitTesting.hs
@@ -3,3 +3,6 @@ import ReadState
 import WriteState
 
 import Test.HUnit
+
+testState = TestList ["Correct Reading 4x4" ~: "a.bb\na.cc\ndddd" ~=? writeState (readState "a.bb\na.cc\ndddd" ),
+                      "Correct Reading 5x4" ~: "a..b\na..b\nad..\n.d..\nccc." ~=? writeState (readState "a..b\na..b\nad..\n.d..\nccc." )]
