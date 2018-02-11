b0ec4eaf024c5e9a4afa3f77899bb3857d14f633
giatrakosg <sdi1600036@di.uoa.gr>
2018-02-11 18:40:55 +0200

    Implement move unit tests

--- unitTesting.hs
+++ unitTesting.hs
@@ -6,3 +6,8 @@ import Test.HUnit
 
 testState = TestList ["Correct Reading 4x4" ~: "a.bb\na.cc\ndddd" ~=? writeState (readState "a.bb\na.cc\ndddd" ),
                       "Correct Reading 5x4" ~: "a..b\na..b\nad..\n.d..\nccc." ~=? writeState (readState "a..b\na..b\nad..\n.d..\nccc." )]
+
+testMove = TestList ["Down" ~: "...\n..a\n..a" ~=? writeState (makeMove (readState "..a\n..a\n...") ('a',South)),
+                     "Right" ~: "...\n.aa\n..." ~=? writeState (makeMove (readState "...\naa.\n...") ('a',East)),
+                     "Left" ~: "...\naa.\n..." ~=? writeState (makeMove (readState "...\n.aa\n...") ('a',West)),
+                     "Up" ~: ".a.\n.a.\n..." ~=? writeState (makeMove (readState "...\n.a.\n.a.") ('a',North))]
