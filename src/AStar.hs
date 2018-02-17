4b7c93a024026c4ecd036913e46c232929775190
giatrakosg <sdi1600036@di.uoa.gr>
2018-02-17 18:41:47 +0200

    Comments

--- src/AStar.hs
+++ src/AStar.hs
@@ -24,16 +24,16 @@ aStar:: (PairingHeap Node) -- Heap of Open nodes
 
 aStar open closed heuristic isFinalFn
                 | (Heap.isEmpty open) = error "Unsolveable Puzzle " -- No Nodes to search
-                | isFinalFn current = stepsTaken current
+                | isFinalFn current = stepsTaken current -- Found final node
                 | isntNew current = aStar open' closed heuristic isFinalFn -- If already seen discard
                 | otherwise = aStar open'' closed' heuristic isFinalFn -- Expand current node , and place
                 -- the result in the closed set
                 where
                     current = Heap.findMin open -- Current node is cheapest in Heap
                     isntNew current = current `Set.member` closed
-                    open' = Heap.deleteMin open
-                    closed' = Set.insert current closed
-                    neighbors = Node.expand current closed heuristic
+                    open' = Heap.deleteMin open -- Delete current from Heap of open
+                    closed' = Set.insert current closed -- Insert current to closed
+                    neighbors = Node.expand current closed heuristic -- Find neighbours of current
                     open'' = visit neighbors open' -- Adds the neighbor nodes to the open set
 
 main = putStrLn $ writeState (makeMoves state (solve_astar state ))
